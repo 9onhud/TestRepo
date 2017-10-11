2e05eac35e44e94a3eb18c7c717b539a8a1897dc
9onhud <toto_2jtb@hotmail.com>
2017-10-11 10:56:11 +0700

    First Test

---
+++ src/main/java/StartApp.java
@@ -0,0 +1,8 @@
+/**
+ * Created by PC301 on 11/10/2560.
+ */
+public class StartApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/testPackages/test.java
@@ -0,0 +1,7 @@
+package testPackages;
+
+/**
+ * Created by PC301 on 11/10/2560.
+ */
+public class test {
+}
